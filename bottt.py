76f5d9c6b660892e8c711192170534f6de60d0e7
mdbasan011 <mdbasan011@gmail.com>
2026-04-01 18:54:41 +0700

    Update bottt.py

--- bottt.py
+++ bottt.py
@@ -82,7 +82,7 @@ BOT_TOKEN = "8415663762:AAH1CZX9hJ_qtIoT_X72UW8DW5uIm5M1P1s"
 ADMIN_ID = 6683331082
 DB_FILE = "ff_bot_data.json"
 TOKEN_LOG = "access_tokens.txt"
-VERSION = "5.6.3"
+VERSION = "5.6.4"
 VIP_CONTACT = "@liggdzut1"
 
 BANK_STK = "0368925982"
